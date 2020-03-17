e354ea6cb8b072aecd865bde092f89e9e0a06e1e
dvvbk <62288682+dvvbk@users.noreply.github.com>
2020-03-17 16:00:26 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `John Rockstar`,
   },
   plugins: [
     {
@@ -25,8 +25,8 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        short_name: `John`,
+        description: `Strona muzyka`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
