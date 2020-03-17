eaa335f40a68ff6c4576490b8b29f37763163686
dvvbk <62288682+dvvbk@users.noreply.github.com>
2020-03-17 17:32:20 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
     siteTitleAlt: `John Rockstar`,
+    siteLanguage: `pl`,
   },
   plugins: [
     {
